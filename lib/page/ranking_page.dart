e2db0309dea073a567f71ae57a35c71feed6364e
ytljl <14921967+ytljl@user.noreply.gitee.com>
2025-06-19 17:15:26 +0800

    实现排行榜功能

--- lib/page/ranking_page.dart
+++ lib/page/ranking_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bilbil_app/http/dao/ranking_dao.dart';
+import 'package:flutter_bilbil_app/page/ranking_tab_page.dart';
+import 'package:flutter_bilbil_app/util/view_util.dart';
+import 'package:flutter_bilbil_app/widget/hi_tab.dart';
+import 'package:flutter_bilbil_app/widget/navigation_bar.dart';
 
 class RankingPage extends StatefulWidget {
   const RankingPage({super.key});
@@ -7,14 +12,68 @@ class RankingPage extends StatefulWidget {
   State<RankingPage> createState() => _RankingPageState();
 }
 
-class _RankingPageState extends State<RankingPage> {
+class _RankingPageState extends State<RankingPage>
+    with TickerProviderStateMixin {
+  static const TARS = [
+    {"key": "like", "name": "最热"},
+    {"key": "pubdate", "name": "最新"},
+    {"key": "favorite", "name": "收藏"}
+  ];
+  late TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: TARS.length, vsync: this);
+    RankingDao.get("like");
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        child: Text("排行榜"),
+      body: Column(
+        children: [_buildNavigationBar(), _buildTabView()],
       ),
+    );
+  }
+
+  _buildNavigationBar() {
+    return NavigationBarPlus(
+      child: Container(
+        decoration: bottomBoxShadow(),
+        alignment: Alignment.center,
+        child: _tabBar(),
+      ),
+    );
+  }
+
+  _buildTabView() {
+    return Flexible(
+      child: TabBarView(
+          controller: _controller,
+          children: TARS.map((tab) {
+            return RankingTabPage(sort: tab['key'] as String);
+          }).toList()),
+    );
+  }
+
+  _tabBar() {
+    return HiTab(
+      TARS.map<Tab>((tab) {
+        return Tab(
+          text: tab['name'],
+        );
+      }).toList(),
+      fontSize: 16,
+      borderWidth: 3,
+      unselectedLabelColor: Colors.black54,
+      controller: _controller,
     );
   }
 }
--- lib/page/ranking_tab_page.dart
+++ lib/page/ranking_tab_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bilbil_app/core/hi_base_tab_state.dart';
+import 'package:flutter_bilbil_app/http/dao/ranking_dao.dart';
+import 'package:flutter_bilbil_app/model/ranking_mo.dart';
+import 'package:flutter_bilbil_app/model/video_model.dart';
+import 'package:flutter_bilbil_app/widget/video_large_card.dart';
 
 class RankingTabPage extends StatefulWidget {
   final String sort;
@@ -8,9 +13,29 @@ class RankingTabPage extends StatefulWidget {
   State<RankingTabPage> createState() => _RankingTabPageState();
 }
 
-class _RankingTabPageState extends State<RankingTabPage> {
+class _RankingTabPageState
+    extends HiBaseTabState<RankingMo, VideoModel, RankingTabPage> {
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  get contentChild => Container(
+        child: ListView.builder(
+          physics: AlwaysScrollableScrollPhysics(),
+          padding: EdgeInsets.only(top: 10),
+          itemCount: dataList.length,
+          controller: scrollController,
+          itemBuilder: (context, index) =>
+              VideoLargeCard(videoModel: dataList[index]),
+        ),
+      );
+
+  @override
+  Future<RankingMo> getData(int pageIndex) async {
+    RankingMo result =
+        await RankingDao.get(widget.sort, pageIndex: pageIndex, pageSize: 20);
+    return result;
+  }
+
+  @override
+  List<VideoModel> parseList(RankingMo result) {
+    return result.list;
   }
 }
