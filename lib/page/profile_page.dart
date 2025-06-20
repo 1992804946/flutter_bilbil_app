d3b85cd94589f9dba34b8808c1684ee0af4a640b
ytljl <14921967+ytljl@user.noreply.gitee.com>
2025-06-20 15:41:53 +0800

    个人中心数据获取搭建

--- lib/page/profile_page.dart
+++ lib/page/profile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bilbil_app/http/core/error.dart';
+import 'package:flutter_bilbil_app/http/dao/profile_dao.dart';
+import 'package:flutter_bilbil_app/model/profile_mo.dart';
+import 'package:flutter_bilbil_app/util/toast.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -7,7 +11,17 @@ class ProfilePage extends StatefulWidget {
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class _ProfilePageState extends State<ProfilePage>
+    with AutomaticKeepAliveClientMixin {
+  ProfileMo? _profileMo;
+  ScrollController _controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,5 +30,24 @@ class _ProfilePageState extends State<ProfilePage> {
         child: Text("我的"),
       ),
     );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+
+  void _loadData() async {
+    try {
+      ProfileMo result = await ProfileDao.get();
+      print(result);
+      setState(() {
+        _profileMo = result;
+      });
+    } on NeedAuth catch (e) {
+      print(e);
+      showWarnToast(e.message);
+    } on HiNetError catch (e) {
+      print(e);
+      showWarnToast(e.message);
+    }
   }
 }
